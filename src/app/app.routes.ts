3cfe7b822ccefc56a78ef45da7da90da1d36a2cc
Alan Solano <alan.solano@itq.edu.ec>
2025-06-30 09:43:41 -0500

    Formulario

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,5 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { FormComponent } from './components/form/form.component';
+export const routes: Routes = [
+    { path: 'form', component: FormComponent },
+];
